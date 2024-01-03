a4024b0e43113e91bd7ef8ba804e4ea4aa2ea31d
Nouh425 <nouhbenchlikha10@gmail.com>
2024-01-03 14:55:41 +0000

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FooterComponent } from './templates/footer/footer.component';
+import { HeaderComponent } from './templates/header/header.component';
+import { BodyComponent } from './templates/body/body.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,FooterComponent,HeaderComponent,BodyComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
